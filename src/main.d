54968dcbb1bc05f6b7cbf1880e50365d3be19d51
Raffa064 <raffaddtg@gmail.com>
2023-07-02 15:06:20 +0000

    ignored main.d and main.o

--- src/main.d
+++
@@ -1,6 +0,0 @@
-src/main.o: src/main.cpp src/./tests/arrays.cpp src/./tests/classes.cpp \
- src/./tests/conditionals.cpp src/./tests/helloworld.cpp \
- src/./tests/input.cpp src/./tests/loops.cpp \
- src/./tests/method-overload.cpp src/./tests/pointers.cpp \
- src/./tests/structs.cpp src/./tests/switches.cpp \
- src/./tests/variables.cpp src/./tests/arithmetic.cpp
